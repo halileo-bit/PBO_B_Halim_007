b2440470f9bea75c5075c97832efaba1b96d1297
Halim Mubdi <halim.dash25@gmail.com>
2025-03-10 07:11:57 +0700

    new file:   Codelab/Codelab1_Modul2/Hewan.class 	new file:   Codelab/Codelab1_Modul2/Hewan.java 	new file:   Codelab/Codelab1_Modul2/Main.java

---
+++ Codelab/Codelab1_Modul2/Hewan.java
@@ -0,0 +1,17 @@
+package Codelab1_Modul2;
+
+public class Hewan {
+    String nama, jenis, suara;
+
+    Hewan(String nama, String jenis, String suara) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.suara = suara;
+    }
+
+    void talk() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis: " + jenis);
+        System.out.println("Suara: " + suara);
+    }
+}
---
+++ Codelab/Codelab1_Modul2/Main.java
@@ -0,0 +1,11 @@
+package Codelab1_Modul2;
+
+public class Main {
+    public static void main(String[] args) {
+        Hewan nama1 = new Hewan("Kucing", "Mamalia", "Nyann~~");
+        Hewan nama2 = new Hewan("Anjing", "Mamalia", "Woof-Woof!!");
+
+        nama1.talk();
+        nama2.talk();
+    }
+}
